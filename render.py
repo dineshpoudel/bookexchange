185b041faeda9e17f2fa6cfcac37ac847049f090
sarjan <shrestha.sarjan@gmail.com>
2013-03-17 14:31:56 -0300

    default argument page title added

--- render.py
+++ render.py
@@ -1,7 +1,7 @@
 import jinja2
 import os
 
-def render(self,file,values={'a':'a'}):
+def render(self,file,values={'mainpageTitle':'Book Exchange'}):
 	self.response.out.write(jinja.get_template(file).render(values))
 
 jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),autoescape=True)
