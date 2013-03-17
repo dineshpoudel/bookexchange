464118c3808415fd9d4c9279bc5994a9164fa16a
sarjan <shrestha.sarjan@gmail.com>
2013-03-17 12:54:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class Book(db.Model):
 	phoneNumber = db.PhoneNumberProperty()
 	email = db.EmailProperty()
 	facebook = db.LinkProperty()
+	remark = db.TextProperty()
 	
 	googleImage = db.LinkProperty()
 	sold = db.BooleanProperty(default=False)
@@ -22,36 +23,43 @@ class Book(db.Model):
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		out = {'mainpageTitle':'Book Exchange'}
+		out = {'mainpageTitle':'Book Exchange','submit':'Add Book','reset':'Reset','resetAction':'reset()'}
 		render(self,'front.html',out)
 	
-	def post(self):		
-		out = {'mainpageTitle':'Book Exchange'}
-		#validating
-		if not self.request.get('bookTitle'):
-			out['bookTitleError'] = 'Book Title cant be empty.'
-		elif not self.request.get('bookCondition'):
-			out['bookConditionError'] = 'Please give a brief description of the condtion of the book.'
-		elif not self.request.get('price'):
-			out['priceError'] = 'Please input the price you expect. Enter 0 if you want to donate.'
-		elif not ( self.request.get('phoneNumber') or self.request.get('email') or self.request.get('facebook')):
-			out['contactError'] = 'Please input at least one method of contacting you.'
-		else:
-			out['contactError'] = 'yay!'
-			#display all data to verify
+	def post(self):
+		if self.request.get('submit') == 'Verify Data':
+			#store data to database
+			self.response.out.write('storing to database')
 		
-		#displayingprevious values like
-		out['bookTitle'] = self.request.get('bookTitle')
-		out['courseID'] = self.request.get('courseID')
-		out['ISBN'] = self.request.get('ISBN')
-		out['edition'] = self.request.get('edition')
-		out['bookCondition'] = self.request.get('bookCondition')
-		out['price'] = self.request.get('price')
-		out['phoneNumber'] = self.request.get('phoneNumber')
-		out['email'] = self.request.get('email')
-		out['facebook'] = self.request.get('facebook')
-		render(self,'front.html',out)
-		
+		else: #validating
+			out = {'mainpageTitle':'Book Exchange','submit':'Add Book','reset':'Reset','resetAction':'reset()'}
+			#validating
+			if not self.request.get('bookTitle'):
+				out['bookTitleError'] = 'Book Title cant be empty.'
+			elif not self.request.get('bookCondition'):
+				out['bookConditionError'] = 'Please give a brief description of the condtion of the book.'
+			elif not self.request.get('price'):
+				out['priceError'] = 'Please input the price you expect. Enter 0 if you want to donate.'
+			elif not ( self.request.get('phoneNumber') or self.request.get('email') or self.request.get('facebook')):
+				out['contactError'] = 'Please input at least one method of contacting you.'
+			else:
+				out['submit'] = 'Verify Data'
+				out['reset'] = 'Edit Again'
+				out['resetAction'] = 'back()'
 
+				#display all data to verify
+		
+			#displayingprevious values like
+			out['bookTitle'] = self.request.get('bookTitle')
+			out['courseID'] = self.request.get('courseID')
+			out['ISBN'] = self.request.get('ISBN')
+			out['edition'] = self.request.get('edition')
+			out['bookCondition'] = self.request.get('bookCondition')
+			out['price'] = self.request.get('price')
+			out['phoneNumber'] = self.request.get('phoneNumber')
+			out['email'] = self.request.get('email')
+			out['facebook'] = self.request.get('facebook')
+			out['remark'] = self.request.get('remark')
+			render(self,'front.html',out)
 			
 app = webapp2.WSGIApplication([('/',MainPage)],debug=True)
