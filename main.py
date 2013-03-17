80feb8cd2ffc179761d71368b0755a4ab073f8cc
sarjan <shrestha.sarjan@gmail.com>
2013-03-16 22:13:48 -0700

    validation included

--- main.py
+++ main.py
@@ -21,9 +21,37 @@ class Book(db.Model):
 	timestamp = db.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp2.RequestHandler):
-
-  def get(self):
-    render(self,'front.html',{'mainpageTitle':'Book Exchange','output':''})
+	def get(self):
+		out = {'mainpageTitle':'Book Exchange'}
+		render(self,'front.html',out)
+	
+	def post(self):		
+		out = {'mainpageTitle':'Book Exchange'}
+		#validating
+		if not self.request.get('bookTitle'):
+			out['bookTitleError'] = 'Book Title cant be empty.'
+		elif not self.request.get('bookCondition'):
+			out['bookConditionError'] = 'Please give a brief description of the condtion of the book.'
+		elif not self.request.get('price'):
+			out['priceError'] = 'Please input the price you expect. Enter 0 if you want to donate.'
+		elif not ( self.request.get('phoneNumber') or self.request.get('email') or self.request.get('facebook')):
+			out['contactError'] = 'Please input at least one method of contacting you.'
+		else:
+			out['contactError'] = 'yay!'
+			#display all data to verify
+		
+		#displayingprevious values like
+		out['bookTitle'] = self.request.get('bookTitle')
+		out['courseID'] = self.request.get('courseID')
+		out['ISBN'] = self.request.get('ISBN')
+		out['edition'] = self.request.get('edition')
+		out['bookCondition'] = self.request.get('bookCondition')
+		out['price'] = self.request.get('price')
+		out['phoneNumber'] = self.request.get('phoneNumber')
+		out['email'] = self.request.get('email')
+		out['facebook'] = self.request.get('facebook')
+		render(self,'front.html',out)
+		
 
 			
 app = webapp2.WSGIApplication([('/',MainPage)],debug=True)
