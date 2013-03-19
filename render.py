119816811aaad9714b2d21fd27f44af99661c4cd
sarjan <shrestha.sarjan@gmail.com>
2013-03-19 12:52:51 -0300

    all imports included here to avoid imports in main

--- render.py
+++ render.py
@@ -1,7 +1,12 @@
 import jinja2
 import os
+import cgi
+import webapp2
+import logging #just for debugging purpose
 
-def render(self,file,values={'mainpageTitle':'Book Exchange'}):
+from google.appengine.ext import db
+
+def render(self,file,values={'junk':'junk'}):
 	self.response.out.write(jinja.get_template(file).render(values))
 
 jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),autoescape=True)
