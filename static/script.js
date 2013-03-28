40902764e00ccc69324377eab1e5f47d9dda7b86
sarjan <shrestha.sarjan@gmail.com>
2013-03-28 15:20:09 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,12 @@
+//hide addbook more details form initially
+$(function(){$(".moreDetailsForm").hide();});
+
+//addbook form toggler
+$(function(){$(".addBookClickable").click(function(){$(".addBookForm").slideToggle();})});
+
+//addbook more details toggler
+$(function(){$(".moreDetailsClickable").click(function(){$(".moreDetailsForm").slideToggle();});});
+
 function imageSelected(link){
 	document.getElementById('googleImage').value = link;
 }
